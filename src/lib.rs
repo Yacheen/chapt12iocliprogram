951fac82066c8c029c1c6d512e602bdbbd4aa1cb
Yassin Moussaoui <yassinmoussaouibusiness@gmail.com>
2022-03-12 16:35:47 -0700

    made test pass

--- src/lib.rs
+++ src/lib.rs
@@ -23,8 +23,16 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>>  {
 
     Ok(())
 }
-pub fn search() {
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut results = Vec::new();
     
+    for line in contents.lines() {
+        if line.contains(query) {
+            results.push(line);
+        }
+    }
+
+    results
 }
 
 
@@ -36,12 +44,12 @@ mod tests {
 
     #[test]
     fn one_result() {
-        let query = "duct";
+        let query = "safe, fast, productive.";
         let contents = "\
-        Rust:
-        safe, fast, productive.
-        Pick three.";
+Rust:
+safe, fast, productive.
+Pick three.";
 
-        assert_eq!(vec!["safe, fast, productive"], search(query, contents)); 
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents)); 
     }
 }
