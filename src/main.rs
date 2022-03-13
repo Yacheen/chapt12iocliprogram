e5ba2513e0bcfd95f9df49c38afd638ad3fc0292
Yassin Moussaoui <yassinmoussaouibusiness@gmail.com>
2022-03-13 06:18:22 -0600

    printing to error instead or std output

--- src/main.rs
+++ src/main.rs
@@ -5,12 +5,13 @@ use chapt12iocliprogram::Config;
 fn main() {
     let args: Vec<String> = env::args().collect();
     let config = Config::new(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {}", err);
+        eprintln!("Problem parsing arguments: {}", err);
+        
         process::exit(1);
     });
 
     if let Err(e) = chapt12iocliprogram::run(config) {
-        println!("Problem getting file: {}", e);
+        eprintln!("Application error: {}", e);
         process::exit(1);
     };
     
